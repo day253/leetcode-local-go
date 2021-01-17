afeac7651a6515d3335ed34eb1b097d4fe24e027
day253 <9634619+day253@users.noreply.github.com>
2021-01-17 15:09:32 +0000

    add test case

--- sort/select.go
+++ sort/select.go
@@ -1,6 +1,7 @@
 package sort
 
 // SelectSort implements the selection sort
+// https://golangbyexample.com/go-selection-sort/
 func SelectSort(a []int) []int {
 	length := len(a)
 	for i := 0; i < length-1; i++ {
