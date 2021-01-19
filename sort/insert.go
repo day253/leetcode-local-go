d19d1ea9b31f82849b464cbba192a79bb16cddef
day253 <9634619+day253@users.noreply.github.com>
2021-01-19 14:38:11 +0000

    add insertion

--- sort/insert.go
+++ sort/insert.go
@@ -1,7 +1,24 @@
 package sort
 
+import "fmt"
+
 // InsertSort etc
 // https://golangbyexample.com/insertion-sort-in-go/
-func InsertSort() {
-
+// best n
+// average n^2
+// worst n^2
+// memory 1
+// stable no
+// method insertion
+func InsertSort(arr []int) {
+	len := len(arr)
+	fmt.Println("[ - ] ", arr)
+	for i := 1; i < len; i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+		fmt.Println("[", i, "] ", arr)
+	}
 }
--- sort/insert_test.go
+++ sort/insert_test.go
@@ -1,1 +1,14 @@
 package sort
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsertSort(t *testing.T) {
+	for _, problem := range problems() {
+		InsertSort(problem.question)
+		assert.Equal(t, problem.answer, problem.question)
+	}
+}
